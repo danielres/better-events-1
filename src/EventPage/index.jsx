b4b4be0cf01a2111091b1020805aac67133450e7
Daniel Reszka <dannn.r@gmail.com>
2017-10-14 20:59:13 +0200

    add hardcoded subjects list to EventPage

--- src/EventPage/index.jsx
+++ src/EventPage/index.jsx
@@ -1,11 +1,33 @@
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import React from 'react';
+
+const subjects = [
+  {
+    id: 'j3hd83hm',
+    name: 'Subject 1',
+  },
+  {
+    id: 'j4sk8ohv',
+    name: 'Subject 2',
+  },
+  {
+    id: 'jss485kl',
+    name: 'Other subject',
+  },
+];
 
 export default ({ eventCode }) => (
   <div>
-    <div>EVENTPAGE</div> <div>event code:{eventCode}</div>
-    <h2>Subjects</h2>
-    <div>Subject 1</div>
-    <div>Subject 2</div>
-    <div>Other subject</div>
+    <h2>
+      [Event name] <small>Event code: {eventCode}</small>
+    </h2>
+    <h3>Subjects</h3>
+    <ListGroup>
+      {subjects.map(subject => (
+        <ListGroupItem href="#" key={subject.id}>
+          {subject.name}
+        </ListGroupItem>
+      ))}
+    </ListGroup>{' '}
   </div>
 );
