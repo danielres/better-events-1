dc31c6af42e316dcddfa38f06dead79131d8cf41
Daniel Reszka <dannn.r@gmail.com>
2017-10-14 20:32:30 +0200

    remove default intro text

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Better Events</h1>
         </header>
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to
-          reload.
-        </p>
 
         {this.props.children}
       </div>
