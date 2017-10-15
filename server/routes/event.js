428ae1c26a7de744732f5c8a6969006b198dce41
Daniel Reszka <dannn.r@gmail.com>
2017-10-15 14:49:22 +0200

    improve event mock in api

--- server/routes/event.js
+++ server/routes/event.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({ event: [{ name: 'Event 1' }] });
+  res.json({
+    event: [
+      {
+        id: 'dhk38s2',
+        name: 'Event 1',
+        subjects: [
+          { name: 'Subject 1' },
+          { name: 'Subject 2' },
+          { name: 'Subject 3' },
+          { name: 'Other' },
+        ],
+      },
+    ],
+  });
 });
 
 module.exports = router;
