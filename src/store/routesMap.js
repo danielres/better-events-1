519c1c6c1fb5b45b00f0cfbd6910cef81b518379
Daniel Reszka <dannn.r@gmail.com>
2017-10-15 14:38:09 +0200

    fetch event at route level

--- src/store/routesMap.js
+++ src/store/routesMap.js
@@ -1,11 +1,30 @@
-import { fetchEvent } from './events';
-import { redirect } from 'redux-first-router';
+import 'whatwg-fetch';
+
+import { types as eventTypes } from './events';
 
 export default {
   HOMEPAGE: '/',
   EVENTPAGE: {
     path: '/:eventCode',
-    thunk: (dispatch, getState) =>
-      fetchEvent(getState().location.payload.eventCode)(dispatch),
+    thunk: (dispatch, getState) => {
+      const { eventCode } = getState().location.payload;
+
+      fetch(`/api/v1/events/${eventCode}`)
+        .then(response => response.json())
+        .then(({ event }) =>
+          dispatch({
+            type: eventTypes.EVENT_SUCCESS,
+            event,
+            eventCode,
+          })
+        )
+        .catch(error =>
+          dispatch({
+            type: eventTypes.EVENT_FAILURE,
+            error,
+            eventCode,
+          })
+        );
+    },
   },
 };
