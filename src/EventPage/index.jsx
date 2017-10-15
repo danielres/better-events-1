9ec6acd7f1ce4678a95a886233b018118fb8d0d2
Daniel Reszka <dannn.r@gmail.com>
2017-10-15 15:03:51 +0200

    populate EventPage with fetched data from api

--- src/EventPage/index.jsx
+++ src/EventPage/index.jsx
@@ -1,33 +1,29 @@
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import React from 'react';
+import { connect } from 'react-redux';
 
-const subjects = [
-  {
-    id: 'j3hd83hm',
-    name: 'Subject 1',
-  },
-  {
-    id: 'j4sk8ohv',
-    name: 'Subject 2',
-  },
-  {
-    id: 'jss485kl',
-    name: 'Other subject',
-  },
-];
-
-export default ({ eventCode }) => (
+const EventPage = ({ eventCode, event }) => (
   <div>
-    <h2>
-      [Event name] <small>Event code: {eventCode}</small>
-    </h2>
-    <h3>Subjects</h3>
-    <ListGroup>
-      {subjects.map(subject => (
-        <ListGroupItem href="#" key={subject.id}>
-          {subject.name}
-        </ListGroupItem>
-      ))}
-    </ListGroup>{' '}
+    {event && (
+      <div>
+        <h2>
+          {event.name} <small>Event code: {eventCode}</small>
+        </h2>
+
+        <ListGroup>
+          {event.subjects.map(subject => (
+            <ListGroupItem href="#" key={subject.id}>
+              {subject.name}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </div>
+    )}
   </div>
 );
+
+const mapState = ({ events, location }) => ({
+  event: events.entries[location.payload.eventCode],
+});
+
+export default connect(mapState)(EventPage);
