e9678013b9d09e8ac42d1fd6486314dd99d439ad
Daniel Reszka <dannn.r@gmail.com>
2017-10-15 17:54:14 +0200

    reduce size of page header

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,33 @@ import './App.css';
 import { Col, Grid, PageHeader, Row } from 'react-bootstrap';
 import Link from 'redux-first-router-link';
 import React, { Component } from 'react';
+import styled from 'react-emotion';
+
+const Header = styled(PageHeader)`
+  margin: 0;
+  padding: 0;
+  h1 {
+    font-size: 18px;
+    margin: 0;
+    padding: 10px 0;
+  }
+  a {
+    color: inherit;
+    &:hover {
+      color: inherit;
+      text-decoration: none;
+      border-bottom: 1px solid gray;
+    }
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <Grid>
-        <PageHeader>
+        <Header>
           <Link to={{ type: 'HOMEPAGE' }}>Better Events</Link>
-        </PageHeader>
+        </Header>
         <Row>
           <Col xs={12} md={12}>
             {this.props.children}
