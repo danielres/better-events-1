683154c7cf30ee52cf610231bbbb630790a62eb8
Daniel Reszka <dannn.r@gmail.com>
2017-10-15 16:46:29 +0200

    turn header title to homepage link

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './App.css';
 
-import React, { Component } from 'react';
 import { Col, Grid, PageHeader, Row } from 'react-bootstrap';
+import Link from 'redux-first-router-link';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <Grid>
-        <PageHeader>Better Events</PageHeader>
+        <PageHeader>
+          <Link to={{ type: 'HOMEPAGE' }}>Better Events</Link>
+        </PageHeader>
         <Row>
           <Col xs={12} md={12}>
             {this.props.children}
