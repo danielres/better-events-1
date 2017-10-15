3b57523e2d3f2ac2f85cb6e7c60dd72519bcad27
Daniel Reszka <dannn.r@gmail.com>
2017-10-15 14:40:00 +0200

    fix event enpoint named events

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var events = require('./routes/events');
+var event = require('./routes/event');
 var users = require('./routes/users');
 
 var app = express();
@@ -13,6 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api/v1/users', users);
-app.use('/api/v1/events/:eventCode', events);
+app.use('/api/v1/events/:eventCode', event);
 
 module.exports = app;
--- server/routes/event.js
+++ server/routes/event.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('/events');
   res.json({ event: [{ name: 'Event 1' }] });
 });
 
