c5edf98568b3c7e75ba56b2a6c3c7b818b1c778b
Daniel Reszka <dannn.r@gmail.com>
2017-10-16 18:52:40 +0200

    add eventCode to event mock

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,18 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/:eventCode', function(req, res, next) {
-  res.json({
-    event: {
-      id: 'dhk38s2',
-      name: 'Event 1',
-      subjects: [
-        { id: 'sd2kj4k', name: 'Subject 1' },
-        { id: 'llkl23p', name: 'Subject 2' },
-        { id: 'kadn3fv', name: 'Subject 3' },
-        { id: 'l349dfk', name: 'Other' },
-      ],
-    },
-  });
+  const eventCode = 'one';
+
+  if (req.params['eventCode'] === eventCode) {
+    res.json({
+      event: {
+        eventCode: 'one',
+        id: 'dhk38s2',
+        name: 'Event 1',
+        subjects: [
+          { id: 'sd2kj4k', name: 'Subject 1' },
+          { id: 'llkl23p', name: 'Subject 2' },
+          { id: 'kadn3fv', name: 'Subject 3' },
+          { id: 'l349dfk', name: 'Other' },
+        ],
+      },
+    });
+  }
 });
 
 module.exports = router;
