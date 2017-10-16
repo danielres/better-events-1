399b3d5981d4259e29be64637f0230491add53e8
Daniel Reszka <dannn.r@gmail.com>
2017-10-16 18:40:59 +0200

    add basic logging middleware to server

--- server/app.js
+++ server/app.js
@@ -12,6 +12,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.all('*', (req, res, next) => {
+  console.log('## ' + req.method + ' ' + req.url);
+  next();
+});
+
 app.use('/api/v1/users', users);
 app.use('/api/v1/events/:eventCode', events);
 
