fdb5494da78ade83d2d8d882b4ae7baed253216b
Daniel Reszka <dannn.r@gmail.com>
2017-10-18 23:11:54 +0200

    fix socket.io server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 var path = require('path');
 
 var events = require('./routes/events');
@@ -19,15 +17,5 @@ app.all('*', (req, res, next) => {
 
 app.use('/api/v1/users', users);
 app.use('/api/v1/events', events);
-
-io.on('connection', function(socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
-  socket.on('message', function(msg) {
-    console.log('message: ' + msg);
-  });
-});
 
 module.exports = app;
